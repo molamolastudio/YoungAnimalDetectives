bd6b908aa6b85695034910cce4684e3fa499f845
Nicholette <nekonekonik@gmail.com>
2015-04-16 11:03:35 +0800

    Partial creation of project.

---
+++ YoungAnimalDetectives/ProjectsListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectsListViewController.swift
+//  YoungAnimalDetectives
+//
+//  Created by Li Jia'En, Nicholette on 15/4/15.
+//  Copyright (c) 2015 Li Jia'En, Nicholette. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
