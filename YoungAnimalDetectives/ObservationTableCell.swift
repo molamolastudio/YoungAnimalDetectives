f47a7fb4622bd455fb8f25d1a83b17f09668c42b
Nicholette <nekonekonik@gmail.com>
2015-04-18 02:23:17 +0800

    Added ObservationTableCell

---
+++ YoungAnimalDetectives/ObservationTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  ObservationTableCell.swift
+//  YoungAnimalDetectives
+//
+//  Created by Li Jia'En, Nicholette on 18/4/15.
+//  Copyright (c) 2015 Li Jia'En, Nicholette. All rights reserved.
+//
+
+import Foundation
