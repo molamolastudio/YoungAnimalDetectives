f5f45d9c1420455f15d59f5a0ec17b6641c29505
Nicholette <nekonekonik@gmail.com>
2015-04-18 08:56:35 +0800

    Added standard ethogram class.

---
+++ YoungAnimalDetectives/StandardEthogram.swift
@@ -0,0 +1,31 @@
+//
+//  StandardEthogram.swift
+//  YoungAnimalDetectives
+//
+//  Created by Li Jia'En, Nicholette on 18/4/15.
+//  Copyright (c) 2015 Li Jia'En, Nicholette. All rights reserved.
+//
+
+import Foundation
+
+class StandardEthogram {
+    var ethogram: Ethogram
+    
+    init() {
+        let movingBS = BehaviourState(name: "Moving", information: "Moving in a specific direction with an aim")
+        let groomingBS = BehaviourState(name: "Grooming", information: "Animal cleaning, licking or stroking itself")
+        let ingestingBS = BehaviourState(name: "Eating/Drinking", information: "Animal ingesting objects or liquids")
+        let restingBS = BehaviourState(name: "Resting", information: "Animal lying or sitting, eyes opened or closed")
+        let notVisibleBS = BehaviourState(name: "Not Visible", information: "Animal in a visually inaccessible location")
+        
+        var ethogram = Ethogram(name: "Animal")
+        ethogram.addBehaviourState(movingBS)
+        ethogram.addBehaviourState(groomingBS)
+        ethogram.addBehaviourState(ingestingBS)
+        ethogram.addBehaviourState(restingBS)
+        ethogram.addBehaviourState(notVisibleBS)
+        
+        self.ethogram = ethogram
+    }
+    
+}
